e3370d880672e96d6a824b799844ba8fd26ff9ab
Edo <pedoprimasaragi@gmail.com>
2025-12-28 16:11:09 +0700

    fix: Remove background-attachment fixed for mobile compatibility

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -498,7 +498,6 @@
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
-        background-attachment: fixed;
         z-index: 0;
     }
     .hero-overlay {
