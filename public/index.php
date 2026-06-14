12574d495ddf58a55f425f288c534affbf35762f
Edo <pedoprimasaragi@gmail.com>
2026-06-14 18:59:12 +0700

    debug: dump raw exception in public/index.php to bypass faulty exception handler

--- public/index.php
+++ public/index.php
@@ -7,4 +7,14 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 }
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
-$app->handleRequest(Request::capture());
+try {
+    $app->handleRequest(Request::capture());
+} catch (\Throwable $e) {
+    http_response_code(500);
+    echo "<h1>RAW EXCEPTION:</h1>";
+    echo "<b>Message:</b> " . $e->getMessage() . "<br>";
+    echo "<b>Class:</b> " . get_class($e) . "<br>";
+    echo "<b>File:</b> " . $e->getFile() . ":" . $e->getLine() . "<br>";
+    echo "<pre>" . $e->getTraceAsString() . "</pre>";
+    exit;
+}
